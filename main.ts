170ba63c2d7ea725b62fd5a17fd4b15fd7b30fdc
apple471296 <71689201+apple471296@users.noreply.github.com>
2020-10-14 11:24:34 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+let list = [4, 3, 2, 1, 0]
+for (let value of list) {
+    led.plot(value, 4)
+    basic.pause(500)
+}
+list = [0, 1, 2, 3, 4]
+for (let value of list) {
+    led.plot(value, 3)
+    basic.pause(500)
+}
+list = [4, 3, 2, 1, 0]
+for (let value of list) {
+    led.plot(value, 2)
+    basic.pause(500)
+}
+list = [0, 1, 2, 3, 4]
+for (let value of list) {
+    led.plot(value, 1)
+    basic.pause(500)
+}
+list = [4, 3, 2, 1, 0]
+for (let value of list) {
+    led.plot(value, 0)
+    basic.pause(500)
+}
